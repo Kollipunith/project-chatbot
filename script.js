7c6afcbd261f7bccb98c09b08b297110e1545068
kollipunith <kollipunith@gmail.com>
2023-04-21 09:05:19 -0600

    hwllo

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ const handleSubmit = async (e) => {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer <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>'
+            'Authorization': 'Bearer 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'
         },
         body: JSON.stringify({
             prompt: userPrompt,
