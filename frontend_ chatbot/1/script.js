e796ed1cf8cfe6d013afde594ec29212e25c941e
kollipunith <kollipunith@gmail.com>
2023-04-21 04:55:29 -0600

    errer

---
+++ frontend_ chatbot/1/script.js
@@ -0,0 +1,128 @@
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
+
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
+
+let loadInterval
+
+function loader(element) {
+    element.textContent = ''
+
+    loadInterval = setInterval(() => {
+        // Update the text content of the loading indicator
+        element.textContent += '.';
+
+        // If the loading indicator has reached three dots, reset it
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
+    }, 300);
+}
+
+function typeText(element, text, prompt) {
+    let index = 0
+
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index)
+            index++
+        } else {
+            clearInterval(interval)
+        }
+    }, 20)
+
+    if (prompt) {
+        element.parentElement.innerHTML += chatStripe(true, '', generateUniqueId(), prompt)
+    }
+}
+
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId, prompt) {
+    return (
+        `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `
+    )
+}
+
+const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const data = new FormData(form)
+
+    // user's chatstripe
+    const userPrompt = data.get('prompt')
+    chatContainer.innerHTML += chatStripe(false, userPrompt)
+
+    // to clear the textarea input 
+    form.reset()
+
+    // bot's chatstripe
+    const uniqueId = generateUniqueId()
+    chatContainer.innerHTML += chatStripe(true, '', uniqueId, userPrompt)
+
+    // to focus scroll to the bottom 
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    // specific message div 
+    const messageDiv = document.getElementById(uniqueId)
+
+    // messageDiv.innerHTML = "..."
+    loader(messageDiv)
+
+    const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer <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>'
+        },
+        body: JSON.stringify({
+            prompt: userPrompt,
+            max_tokens: 60
+        })
+    })
+
+    clearInterval(loadInterval)
+    messageDiv.innerHTML = " "
+
+    if (response.ok) {
+        const data = await response.json();
+        const parsedData = data.choices[0].text.trim() // trims any trailing spaces/'\n' 
+
+        typeText(messageDiv, parsedData, false)
+
+    } else {
+        const err = await response.text()
+
+        messageDiv.innerHTML = "Something went wrong"
+        alert(err)
+    }
+}
+
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        handleSubmit(e)
+    }
+})
---
+++ frontend_ chatbot/1/script2.js
@@ -0,0 +1,39 @@
+document.getElementById('prompt_form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const userInputElement = document.querySelector('textarea[name="user_input"]');
+    const userInput = userInputElement.value;
+    const promptContainer = document.getElementById('prompt_container');
+
+    try {
+        const response = await fetch('https://api.openai.com/v1/engines/text-davinci-002/completions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer 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'
+            },
+            body: JSON.stringify({
+                prompt: userInput,
+                max_tokens: 60
+            })
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            const generatedPrompt = data.choices[0].text.trim();
+
+         promptContainer.innerHTML += `
+    <div class="message sender"><img src="sender.jpg" alt="sender avatar" /><p><strong>User input:</strong> ${userInput}</p></div>
+    <div class="message receiver"><p><strong>Generated prompt:</strong> ${generatedPrompt}</p><img src="receiver.jpeg" alt="receiver avatar" /></div>
+`;
+        } else {
+            const errorText = await response.text();
+            console.error('API Error:', errorText);
+            alert('Something went wrong. Please check the browser console for more details.');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Something went wrong. Please check the browser console for more details.');
+    }
+});
+
