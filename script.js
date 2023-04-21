89e209616d2fbfb7c67bb7fe83363da2c7b22642
kollipunith <kollipunith@gmail.com>
2023-04-21 09:18:19 -0600

    sdfsd

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ const handleSubmit = async (e) => {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer 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'
+            'Authorization': 'Bearer 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'
         },
         body: JSON.stringify({
             prompt: userPrompt,
